f2e964e38f854952e37df483aaee06224e9cc65f
ElenaSozonova <sozonova.ea@yandex.ru>
2020-04-12 20:27:47 +0300

    вариант с radiobutton

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(775, 425);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown_1);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove_1);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(166, 457);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(110, 21);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(25, 457);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 490);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,134 @@ namespace DreamTeamPaint
 {
     public partial class Form1 : Form
     {
+
+        Color PixelColor;
+
         public Form1()
         {
             InitializeComponent();
         }
-    }
+
+
+        Bitmap q;
+        Bitmap q1;
+        int X;
+        int Y;
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+           q = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+           q1 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+           checkBox1.Checked = false;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            checkBox1.Checked = false;
+        }
+
+        private void pictureBox1_MouseMove_1(object sender, MouseEventArgs e)
+
+        {
+            int X1 = e.X;
+            int Y1 = e.Y;
+
+            //if ((e.X > pictureBox1.Width) || (e.Y > pictureBox1.Height) || (e.X < 0) || (e.Y < 0))
+            //{
+            //    radioButton1.Checked = true;
+            //    X = e.X;
+            //    Y = e.Y;
+            //}
+            //else
+            //{
+            //    radioButton1.Checked = false;
+            //    
+            //}
+
+            if (checkBox1.Checked == true && e.X < pictureBox1.Width && e.Y < pictureBox1.Height && e.X > 0 && e.Y > 0/* && radioButton1.Checked == false*/)
+
+            {
+                q1.SetPixel(X1, Y1, Color.Red);
+                pictureBox1.Image = q1;
+
+                while ((X != X1 || Y != Y1) /*&& (radioButton1.Checked == false)*/)
+                {
+                    if (X < X1 && Y > Y1) //1   
+                    {
+                        q1.SetPixel(X + 1, Y - 1, Color.Red);
+                        pictureBox1.Image = q1;
+                        X = X + 1;
+                        Y = Y - 1;
+                    }
+
+                    if (X > X1 && Y < Y1)//2
+                    {
+                        q1.SetPixel(X - 1, Y + 1, Color.Red);
+                        pictureBox1.Image = q1;
+                        X = X - 1;
+                        Y = Y + 1;
+                    }
+
+                    if (X == X1 && Y <= Y1) //3
+                    {
+                        q1.SetPixel(X, Y + 1, Color.Red);
+                        pictureBox1.Image = q1;
+                        Y = Y + 1;
+
+                    }
+
+                    if (X < X1 && Y == Y1) //4
+                    {
+                        q1.SetPixel(X + 1, Y, Color.Red);
+                        pictureBox1.Image = q1;
+                        X = X + 1;
+                    }
+
+                    if (X > X1 && Y > Y1) //5
+                    {
+                        q1.SetPixel(X - 1, Y - 1, Color.Red);
+                        pictureBox1.Image = q1;
+                        X = X - 1;
+                        Y = Y - 1;
+                    }
+
+                    if (X < X1 && Y < Y1) //6
+                    {
+                        q1.SetPixel(X + 1, Y + 1, Color.Red);
+                        pictureBox1.Image = q1;
+                        X = X + 1;
+                        Y = Y + 1;
+                    }
+
+                    if (X == X1 && Y > Y1) //7
+                    {
+                        q1.SetPixel(X, Y - 1, Color.Red);
+                        pictureBox1.Image = q1;
+                        Y = Y - 1;
+                    }
+
+                    if (X > X1 && Y == Y1) //8
+                    {
+                        q1.SetPixel(X - 1, Y, Color.Red);
+                        pictureBox1.Image = q1;
+                        X = X - 1;
+                    }
+                }
+                X = X1;
+                Y = Y1;
+            }
+        }
+
+       
+
+        private void pictureBox1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            checkBox1.Checked = true;
+            radioButton1.Checked = false;
+            X = e.X;
+            Y = e.Y;
+        }
+    }   
 }
+
+
