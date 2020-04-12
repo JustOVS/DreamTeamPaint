ea8126a87030844cd9d473c553b4d17220c959da
Oleg <olegshitikov@gmail.com>
2020-04-12 18:57:54 +0300

    Добавил класс Painter

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,53 @@ namespace DreamTeamPaint
         {
             InitializeComponent();
         }
+
+
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Painter.draw(e, pictureBox1);
+            //Painter.x1 = e.X;
+            //Painter.y1 = e.Y;
+
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Painter.drawStartFinishFlag = true;
+            Painter.prevX = e.X;
+            Painter.prevY = e.Y;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Painter.pointer = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Painter.drawStartFinishFlag = false;
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+
+            
+        }
     }
 }
